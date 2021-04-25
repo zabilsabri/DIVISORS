e9c96da40eed2a5b0c4b6df5225d08c30656894b
Zabil Sabri Muhammad <79674049+zabilsabri@users.noreply.github.com>
2021-04-26 06:00:11 +0800

    Update DIVISOR.py

--- DIVISOR.py
+++ DIVISOR.py
@@ -1,11 +1,11 @@
-__foundby__ = zabilsabri
+__foundby__ = "zabilsabri"
 
 
 number = int(input("ENTER YOUR NUMBER: "))
 answer = []
 
-for i in range(1, a + 1):
-    x = a % i
+for i in range(1, number + 1):
+    x = number % i
     if x == 0:
         answer.append(i)
 
