bf83d011b9c0c14dd80b1dc346fda9d3b77f181d
Zabil Sabri Muhammad <79674049+zabilsabri@users.noreply.github.com>
2021-06-22 17:54:25 +0800

    shorten the code

--- DIVISOR.py
+++ DIVISOR.py
@@ -5,8 +5,7 @@ number = int(input("ENTER YOUR NUMBER: "))
 answer = []
 
 for i in range(1, number + 1):
-    x = number % i
-    if x == 0:
+    if number % i == 0:
         answer.append(i)
-
+        
 print(answer)
